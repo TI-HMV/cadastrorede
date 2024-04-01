eae72ab6a98ab44cd601fc90cc31c8fcb92105dd
gabrielmaf <gabrielmaf08@gmail.com>
2024-04-01 06:41:38 -0300

    nome da pagina index alterado

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (isset($_GET['mensagem'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Wifi</title>
     <!-- Inclua os arquivos do Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
